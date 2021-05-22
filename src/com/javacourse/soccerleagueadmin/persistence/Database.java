5b5a5eaf42dfcc13616912ab99ed460715ffd722
yanira <dilma.guayacan@runt.com.co>
2021-05-22 11:26:47 -0500

    creacion metodo encontrar jugador por id

--- src/com/javacourse/soccerleagueadmin/persistence/Database.java
+++ src/com/javacourse/soccerleagueadmin/persistence/Database.java
@@ -1,4 +1,19 @@
 package com.javacourse.soccerleagueadmin.persistence;
 
+import java.util.List;
+
+import com.javacourse.soccerleagueadmin.dto.Player;
+
 public class Database {
+	
+	private List<Player> lista;
+	
+	public Player findPlayer(Integer id) {
+		for (Player player : lista) {
+			if(player.getId()==id) {
+				return player;
+			}
+		}
+		return null;
+	}
 }
